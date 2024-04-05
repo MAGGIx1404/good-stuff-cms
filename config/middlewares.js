2d8359c73f2d590728f837bc53bc94b175e9c046
Yash Ramoliya <yashramoliya12@gmail.com>
2024-04-05 23:15:17 +0530

    feat: add cloudinary setup

--- config/middlewares.js
+++ config/middlewares.js
@@ -9,4 +9,17 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', `res.cloudinary.com`],
+          'media-src': ["'self'", 'data:', 'blob:', `res.cloudinary.com`],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
